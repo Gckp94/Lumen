dfcf097c22d94c3ac656026bc0d9c0f24c865038
omoxo <95348476+Gckp94@users.noreply.github.com>
2026-01-14 18:09:48 +0800

    chore: export calculate_percentile_bounds from utils package

--- src/ui/utils/__init__.py
+++ src/ui/utils/__init__.py
@@ -1,5 +1,6 @@
 """UI utility functions."""
 
 from src.ui.utils.number_format import format_number_abbreviated
+from src.ui.utils.percentile import calculate_percentile_bounds
 
-__all__ = ["format_number_abbreviated"]
+__all__ = ["format_number_abbreviated", "calculate_percentile_bounds"]
